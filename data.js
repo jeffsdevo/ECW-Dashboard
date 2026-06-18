172e4efbdb506b066839a34a021795507072bbe3
jeffsdevo <jeffsdevo@gmail.com>
2026-06-18 19:54:01 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,359 @@
+const ECW_DATA = [
+  {
+    "weekOf": "2026-02-23",
+    "medNum": "Book I 1",
+    "title": "Read Your Bible",
+    "rowStatus": "posted",
+    "platforms": {
+      "youtube": null,
+      "morningReading": null,
+      "quoteCard": null,
+      "linkedinNewsletter": null,
+      "linkedin": null,
+      "facebookDeepDive": null,
+      "instagramReel": null,
+      "carousel": "Theonas of Alexandria",
+      "facebookReflection": null
+    }
+  },
+  {
+    "weekOf": "2026-03-02",
+    "medNum": "Book I 2",
+    "title": "Origen on Predestination",
+    "rowStatus": "posted",
+    "platforms": {
+      "youtube": null,
+      "morningReading": null,
+      "quoteCard": null,
+      "linkedinNewsletter": null,
+      "linkedin": null,
+      "facebookDeepDive": null,
+      "instagramReel": null,
+      "carousel": "Celsus",
+      "facebookReflection": null
+    }
+  },
+  {
+    "weekOf": "2026-03-09",
+    "medNum": "Book I 3",
+    "title": "The Unity... Apostolic Origin...",
+    "rowStatus": "posted",
+    "platforms": {
+      "youtube": null,
+      "morningReading": null,
+      "quoteCard": null,
+      "linkedinNewsletter": null,
+      "linkedin": null,
+      "facebookDeepDive": null,
+      "instagramReel": null,
+      "carousel": "Tertullian",
+      "facebookReflection": null
+    }
+  },
+  {
+    "weekOf": "2026-03-16",
+    "medNum": "Book I 4",
+    "title": "Deadly Drinks... Poisonous Writing",
+    "rowStatus": "posted",
+    "platforms": {
+      "youtube": null,
+      "morningReading": null,
+      "quoteCard": null,
+      "linkedinNewsletter": null,
+      "linkedin": null,
+      "facebookDeepDive": null,
+      "instagramReel": null,
+      "carousel": "Augustine of Hippo",
+      "facebookReflection": null
+    }
+  },
+  {
+    "weekOf": "2026-03-23",
+    "medNum": "Book I Jan",
+    "title": "Epiphany",
+    "rowStatus": "posted",
+    "platforms": {
+      "youtube": null,
+      "morningReading": null,
+      "quoteCard": null,
+      "linkedinNewsletter": null,
+      "linkedin": null,
+      "facebookDeepDive": null,
+      "instagramReel": null,
+      "carousel": "Leo the Great",
+      "facebookReflection": null
+    }
+  },
+  {
+    "weekOf": "2026-03-30",
+    "medNum": "Book I 5",
+    "title": "...no Justification for Doing Evil",
+    "rowStatus": "posted",
+    "platforms": {
+      "youtube": null,
+      "morningReading": null,
+      "quoteCard": null,
+      "linkedinNewsletter": null,
+      "linkedin": null,
+      "facebookDeepDive": null,
+      "instagramReel": null,
+      "carousel": "John Wesley",
+      "facebookReflection": null
+    }
+  },
+  {
+    "weekOf": "2026-04-06",
+    "medNum": "Book I Feb",
+    "title": "February - Candlemas",
+    "rowStatus": "posted",
+    "platforms": {
+      "youtube": null,
+      "morningReading": null,
+      "quoteCard": null,
+      "linkedinNewsletter": null,
+      "linkedin": null,
+      "facebookDeepDive": null,
+      "instagramReel": null,
+      "carousel": "Etheria of Galicia",
+      "facebookReflection": null
+    }
+  },
+  {
+    "weekOf": "2026-04-13",
+    "medNum": "Book I 6",
+    "title": "Saved by Grace, Not Works",
+    "rowStatus": "posted",
+    "platforms": {
+      "youtube": null,
+      "morningReading": null,
+      "quoteCard": null,
+      "linkedinNewsletter": null,
+      "linkedin": null,
+      "facebookDeepDive": null,
+      "instagramReel": null,
+      "carousel": "Paul of Tarsus",
+      "facebookReflection": null
+    }
+  },
+  {
+    "weekOf": "2026-04-20",
+    "medNum": "Book I 7",
+    "title": "To Do and Not To Do",
+    "rowStatus": "posted",
+    "platforms": {
+      "youtube": null,
+      "morningReading": null,
+      "quoteCard": null,
+      "linkedinNewsletter": null,
+      "linkedin": null,
+      "facebookDeepDive": null,
+      "instagramReel": null,
+      "carousel": "John Chrysostom",
+      "facebookReflection": null
+    }
+  },
+  {
+    "weekOf": "2026-04-27",
+    "medNum": "Book I 9",
+    "title": "All Languages are God's Languages",
+    "rowStatus": "posted",
+    "platforms": {
+      "youtube": null,
+      "morningReading": null,
+      "quoteCard": null,
+      "linkedinNewsletter": null,
+      "linkedin": null,
+      "facebookDeepDive": null,
+      "instagramReel": null,
+      "carousel": "Origen of Alexandria",
+      "facebookReflection": null
+    }
+  },
+  {
+    "weekOf": "2026-05-04",
+    "medNum": null,
+    "title": null,
+    "rowStatus": "posted",
+    "platforms": {
+      "youtube": null,
+      "morningReading": null,
+      "quoteCard": "Ignatius of Antioch",
+      "linkedinNewsletter": null,
+      "linkedin": null,
+      "facebookDeepDive": null,
+      "instagramReel": null,
+      "carousel": "Gregory Thaumaturgus",
+      "facebookReflection": null
+    }
+  },
+  {
+    "weekOf": "2026-05-11",
+    "medNum": null,
+    "title": null,
+    "rowStatus": "posted",
+    "platforms": {
+      "youtube": null,
+      "morningReading": "Origen",
+      "quoteCard": "Origen",
+      "linkedinNewsletter": null,
+      "linkedin": null,
+      "facebookDeepDive": "Origen",
+      "instagramReel": null,
+      "carousel": "Ephrem the Syrian",
+      "facebookReflection": "Morning Reading Promo"
+    }
+  },
+  {
+    "weekOf": "2026-05-17",
+    "medNum": null,
+    "title": null,
+    "rowStatus": "posted",
+    "platforms": {
+      "youtube": null,
+      "morningReading": "Gregory Thaumaturgus",
+      "quoteCard": "Gregory Thaumaturgus",
+      "linkedinNewsletter": null,
+      "linkedin": null,
+      "facebookDeepDive": "Ephrem the Syrian",
+      "instagramReel": null,
+      "carousel": "Ignatius of Antioch",
+      "facebookReflection": null
+    }
+  },
+  {
+    "weekOf": "2026-05-24",
+    "medNum": "Book III 52",
+    "title": "A Renewed Mind",
+    "rowStatus": "posted",
+    "platforms": {
+      "youtube": null,
+      "morningReading": "Boethius",
+      "quoteCard": "Boethius",
+      "linkedinNewsletter": null,
+      "linkedin": "1st post",
+      "facebookDeepDive": "Boethius",
+      "instagramReel": null,
+      "carousel": "Boethius",
+      "facebookReflection": null
+    }
+  },
+  {
+    "weekOf": "2026-05-31",
+    "medNum": "Book III 51",
+    "title": "Struggle in Adversity",
+    "rowStatus": "posted",
+    "platforms": {
+      "youtube": null,
+      "morningReading": "Cyprian of Carthage",
+      "quoteCard": "Cyprian of Carthage",
+      "linkedinNewsletter": null,
+      "linkedin": "2nd post",
+      "facebookDeepDive": "Cyprian of Carthage",
+      "instagramReel": null,
+      "carousel": "Cyprian of Carthage",
+      "facebookReflection": null
+    }
+  },
+  {
+    "weekOf": "2026-06-07",
+    "medNum": "Book III 50",
+    "title": "God is Not Worshipped in Food",
+    "rowStatus": "posted",
+    "platforms": {
+      "youtube": null,
+      "morningReading": "Novatian",
+      "quoteCard": "Novatian",
+      "linkedinNewsletter": null,
+      "linkedin": "done",
+      "facebookDeepDive": "Novatian",
+      "instagramReel": null,
+      "carousel": "Novatian",
+      "facebookReflection": "Victor Frankl"
+    }
+  },
+  {
+    "weekOf": "2026-06-14",
+    "medNum": "Book III 49",
+    "title": "The Promised Land, Grace Not Law",
+    "rowStatus": "scheduled",
+    "platforms": {
+      "youtube": "Why I wrote ECW",
+      "morningReading": "John Chrysostom",
+      "quoteCard": "John Chrysostom",
+      "linkedinNewsletter": "1st newsletter",
+      "linkedin": "ready",
+      "facebookDeepDive": "John Chrysostom",
+      "instagramReel": null,
+      "carousel": "John Chrysostom",
+      "facebookReflection": null
+    }
+  },
+  {
+    "weekOf": "2026-06-21",
+    "medNum": "Book I 11",
+    "title": "Special Days",
+    "rowStatus": "scheduled",
+    "platforms": {
+      "youtube": "Who is ECW for?",
+      "morningReading": "Hippolytus",
+      "quoteCard": "Hippolytus",
+      "linkedinNewsletter": null,
+      "linkedin": "ready",
+      "facebookDeepDive": "Hippolytus",
+      "instagramReel": null,
+      "carousel": "Hippolytus",
+      "facebookReflection": null
+    }
+  },
+  {
+    "weekOf": "2026-06-28",
+    "medNum": "Book I 19",
+    "title": "Luck and Fortune",
+    "rowStatus": "scheduled",
+    "platforms": {
+      "youtube": "How ECW is set up",
+      "morningReading": "Dionysius of Alexandria",
+      "quoteCard": "Dionysius of Alexandria",
+      "linkedinNewsletter": null,
+      "linkedin": "ready",
+      "facebookDeepDive": "Dionysius of Alexandria",
+      "instagramReel": null,
+      "carousel": "Dionysius of Alexandria",
+      "facebookReflection": null
+    }
+  },
+  {
+    "weekOf": "2026-07-05",
+    "medNum": "Book I 1",
+    "title": "Read Your Bible",
+    "rowStatus": "not_scheduled",
+    "platforms": {
+      "youtube": "Why you want ECW",
+      "morningReading": "Theonas of Alexandria",
+      "quoteCard": "Theonas of Alexandria",
+      "linkedinNewsletter": null,
+      "linkedin": null,
+      "facebookDeepDive": "Theonas of Alexandria",
+      "instagramReel": null,
+      "carousel": "Theonas of Alexandria",
+      "facebookReflection": null
+    }
+  },
+  {
+    "weekOf": "2026-07-12",
+    "medNum": "Book III 46",
+    "title": "Freedom to Reject the Better Way",
+    "rowStatus": "not_scheduled",
+    "platforms": {
+      "youtube": null,
+      "morningReading": "Methodius of Olympus",
+      "quoteCard": "Methodius of Olympus",
+      "linkedinNewsletter": null,
+      "linkedin": null,
+      "facebookDeepDive": "Methodius of Olympus",
+      "instagramReel": null,
+      "carousel": "Methodius of Olympus",
+      "facebookReflection": null
+    }
+  }
+];
